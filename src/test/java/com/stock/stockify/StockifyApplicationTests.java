117c8c666f84fb0e8bac0227ac12d769f509fc5d
wonjae <dnjswo521@gmail.com>
2025-04-21 03:43:57 +0900

    Initial commit

---
+++ src/test/java/com/stock/stockify/StockifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.stockify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
